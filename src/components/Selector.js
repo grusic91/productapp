5f109674fdf48c22af04f126cdae36fdf83eb450
Gruzic <28716217+grusic91@users.noreply.github.com>
2020-07-09 11:21:11 +0200

    Adds url routing

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,39 +1,69 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router, 
+  Route,
+  Switch,
+  Redirect,
+ } from 'react-router-dom';
+import { ToggleLink } from '../routing/ToggleLink';
+import { RoutedDisplay } from '../routing/RoutedDisplay';
 
-export class Selector extends React.Component {   
-    state = {
-        selection: "Products"
-    }   
+//const RouteInfoHOC = withRouter(RouteInfo)
 
-    setSelection = ev => {
-        ev.persist();
-        this.setState({ selection: ev.target.name });
+export class Selector extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showPrompt: false,
+      message: "",
+      callback: () => {}
     }
+  }
 
-    render() {
-        
-        return <div className="container-fluid">
-            <div className="row">
-                <div className="col-2">
-                    {
-                        React.Children.map(this.props.children, c => <button
-                            name={c.props.name}
-                            onClick={this.setSelection}
-                            className={`btn btn-block m-1 ${this.state.selection === c.props.name ?
-                            "btn-primary active" : "btn-secondary"
-                            }`}
-                        >
-                            {c.props.name}
-                        </button>)
-                    }
-                </div>
-                <div className="col">
-                    {
-                        React.Children.toArray(this.props.children)
-                            .filter(c => c.props.name === this.state.selection)
-                    }
-                </div>
-            </div>
+  customGetUserConfirmation = (message, navCallback) => {
+    this.setState({
+      showPrompt: true,
+      message: message,
+      callback: (allow) => {
+        navCallback(allow);
+        this.setState({ showPrompt: false})}
+    });
+  }
+
+  //renderMessage = (msg) => <h5 className="bg-info text-white m-2 p-2">{msg}</h5>
+  render() {
+    const routes = React.Children.map(this.props.children, child => ({
+      component: child,
+      name: child.props.name,
+      url: `/${child.props.name.toLowerCase()}`,
+      dataType: child.props.dataType
+    }));
+
+  return (
+    <Router getUserConfirmation={this.customGetUserConfirmation}>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-2">
+          {
+            routes.map(r => <ToggleLink key={r.url} to={r.url}>{r.name}</ToggleLink>)
+          }
+          </div>
+          <div className="col">
+    
+            <Switch>
+            {
+              routes.map( r => <Route 
+                key={r.url} 
+                path={`/:datatype(${r.dataType})/:model?/:id?`} 
+                component={RoutedDisplay(r.datatype)} 
+                />
+              )
+            }
+             <Redirect to={routes[0].url} />
+            </Switch>            
+          </div>
         </div>
-    }
+      </div>
+    </Router>
+  )}
 }
--- src/components/supplier/SupplierTable.js
+++ src/components/supplier/SupplierTable.js
@@ -4,12 +4,7 @@ import { SupplierTableRow } from './SupplierTableRow';
 export class SupplierTable extends React.Component {
     render() {
         return <table className="table table-sm table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th colSpan="5" className="bg-primary text-white text-center h4 p-2">
-                        Suppliers
-                    </th>
-                </tr>            
+            <thead>      
                 <tr>
                     <th>ID</th><th>Name</th><th>City</th><th>Products</th>
                 </tr>
